057e892c6b13b48dd28360cdd09860945a8a1c16
Mikita Kamiakevich <79355999+nikitsch@users.noreply.github.com>
2023-06-11 18:18:00 +0300

    feat: connect to git (#2)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,8 @@
-
+import { useSearchUsersQuery } from "../store/github/github.api"
 
 export function HomePage() {
+  const {isLoading, isError, data} = useSearchUsersQuery('nikitsch')
+
   return (
     <div>home</div>
   )
