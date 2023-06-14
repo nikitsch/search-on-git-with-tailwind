bbb9dbaa3ea6383c7e8488f0d683061522ac8ad8
nikitsch <nikitsch5@gmail.com>
2023-06-14 13:43:39 +0300

    feat: filling favourite page

--- src/hooks/debounce.ts
+++ src/hooks/debounce.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export function useDebounce(value: string, delay: number = 300): string {
+export function useDebounce(value: string, delay: number = 600): string {
   const [debounced, setDebounced] = useState(value)
 
   useEffect(() => {
